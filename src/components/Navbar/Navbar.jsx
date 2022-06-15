f20a3be9066d95e48644c4ba183bbf698943eb7c
mitul-garg <mitulgarg2000@gmail.com>
2022-06-15 22:08:47 +0530

    Navbar Skeleton ready

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import logo from "../../assests/logo.webp";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
+
+import "./styles.css";
+
+const Navbar = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const toggleDrawer = () => setOpenDrawer((prev) => !prev);
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="left-nav">
+          <img src={logo} alt="Toralabs" className="nav-logo" />
+        </div>
+        <div className="right-nav">
+          <div className="drawer-icons">
+            <GiHamburgerMenu
+              className={openDrawer ? "hamburger hidden" : "hamburger"}
+              onClick={toggleDrawer}
+            />
+            <GrClose
+              className={openDrawer ? "close-icon" : "close-icon hidden"}
+              onClick={toggleDrawer}
+            />
+          </div>
+          <ul className="nav-links">
+            <li>About</li>
+            <li>Apps</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+      <ul className={openDrawer ? "down-drawer" : "down-drawer hidden"}>
+        <li>About</li>
+        <li>Apps</li>
+        <li>Contact</li>
+      </ul>
+    </>
+  );
+};
+
+export default Navbar;
