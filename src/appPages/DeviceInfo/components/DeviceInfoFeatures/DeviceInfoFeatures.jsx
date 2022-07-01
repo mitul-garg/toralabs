a3795da15d393a5bbc2a0b39b186e6d0621f01c4
mitul-garg <mitulgarg2000@gmail.com>
2022-07-01 14:14:54 +0530

    DeviceInfo - More Features Added

--- src/appPages/DeviceInfo/components/DeviceInfoFeatures/DeviceInfoFeatures.jsx
+++ src/appPages/DeviceInfo/components/DeviceInfoFeatures/DeviceInfoFeatures.jsx
@@ -3,6 +3,13 @@ import React from "react";
 import { deviceInfoFeatures } from "../../../../data/deviceInfoFeatures";
 import { IoLogoYoutube } from "react-icons/io5";
 
+import image_memory from "../../../../assets/device-info/features/image_memory.png";
+import image_network from "../../../../assets/device-info/features/image_network.png";
+import image_widget from "../../../../assets/device-info/features/image_widget.png";
+import image_export_details from "../../../../assets/device-info/features/image_export_details.png";
+import image_colorful_collage from "../../../../assets/device-info/features/image_colorful_collage.png";
+import image_language_collage from "../../../../assets/device-info/features/image_language_collage.png";
+
 import "./styles.css";
 
 const DeviceInfoFeatures = () => {
@@ -26,6 +33,114 @@ const DeviceInfoFeatures = () => {
           </div>
         ))}
       </div>
+
+      <div className="divider" />
+
+      <div className="features-desc-ss-holder">
+        <div className="feature-ss-container">
+          <img src={image_memory} className="feature-ss-img ss-img-mobile" />
+          <div className="feature-ss-content">
+            <h2>Memory Info</h2>
+            <p>
+              This app is unique among all other device info apps, as we provide
+              Z-RAM details on approximately every smartphone. Other than that,
+              we show live RAM consumption status, System Storage, Internal
+              Storage, External Storage. Also, we show info regarding RAM type
+              and its bandwidth and channels. We are constantly updating our
+              database to include more devices for this info.
+            </p>
+          </div>
+          <img src={image_memory} className="feature-ss-img ss-img-desktop" />
+        </div>
+        <div className="feature-ss-container">
+          <img src={image_network} className="feature-ss-img" />
+          <div className="feature-ss-content">
+            <h2>Network Info</h2>
+            <p>
+              View info related to your Wi-fi or Network. We also provide the
+              SIM info. Get details about your IPv4 address, IPv6 address, MAC
+              address, Subnet masks, Gateway, Link Speed, Signal Strength,
+              Frequency and much more. If you want more features on this
+              particular section, connect with us. We would love to hear your
+              feedback.
+            </p>
+          </div>
+        </div>
+        <div className="feature-ss-container">
+          <img src={image_widget} className="feature-ss-img ss-img-mobile" />
+          <div className="feature-ss-content">
+            <h2>Detailed Widgets Description</h2>
+            <p>
+              Do you want to know what changes on your smartphone on the go.
+              Introducing a brand new widgets that has the Battery Temperature,
+              Voltage, RAM, Internal Storage, System Storage, CPU Usage and
+              current CPU Frequency in MHz. Along with that, you can update the
+              data in widgets anytime, with a single click on Refresh Button.
+              Widgets automatically updates in every 30 min.
+            </p>
+          </div>
+          <img src={image_widget} className="feature-ss-img ss-img-desktop" />
+        </div>
+        <div className="feature-ss-container">
+          <img src={image_export_details} className="feature-ss-img" />
+          <div className="feature-ss-content">
+            <h2>Export Data (Text/PDF)</h2>
+            <p>
+              Now you can export all the information shown by Device info to a
+              PDF file or a Text file. Customize what information is needed to
+              export and then you can share it with your friends. Also, you can
+              use this document as a Professional document of your device. Note
+              that, we support many languages chosen from language picker while
+              exporting data to a Text file, but while exporting in PDF format
+              we only support English at the present moment.
+            </p>
+          </div>
+        </div>
+        <div className="feature-ss-container">
+          <img
+            src={image_colorful_collage}
+            className="feature-ss-img ss-img-mobile"
+          />
+          <div className="feature-ss-content">
+            <h2>Global Language Support</h2>
+            <p>
+              Do you speak English? No. Don't worry. We support 15 major global
+              languages to show the data. The languages supported by us are
+              عربي, 中国人, Nederlands, English, français, Deutsche, Ελληνικά,
+              हिन्दी, Bahasa Indonesia, Italiano, 日本語, Português, русский,
+              Español, Tiếng Việt. If you're unable to find your native
+              language, connect on email and we will add it asap. Also, you can
+              contribute in translations of the strings and get your name
+              featured in the app.
+            </p>
+          </div>
+          <img
+            src={image_colorful_collage}
+            className="feature-ss-img ss-img-desktop"
+          />
+        </div>
+        <div className="feature-ss-container">
+          <img
+            src={image_language_collage}
+            className="feature-ss-img collage"
+          />
+          <div className="feature-ss-content">
+            <h2>Perform Service Tests</h2>
+            <p>
+              Thinking if the hardware of your device is operating perfectly?
+              You have never bothered to check them right? Don't worry, we have
+              got you covered. Device Info can test your smartphone hardware for
+              faults and consistencies. As for now, it supports tests for
+              Display, Multi-touch, Flashlight, Loudspeaker, Ear Speaker, Ear
+              Proximity, Light Sensor, Accelerometer, Vibration, Wi-Fi,
+              Bluetooth, Fingerprint, Volume Buttons. These tests require few
+              permissions to work effectively. So, grant the permissions if
+              asked and check your device in and out.
+            </p>
+          </div>
+        </div>
+      </div>
+
       <a
         href="https://www.youtube.com/watch?v=iIRDG23SS6s"
         target="_blank"
